856e0c391e49c6b8f752d42e1d0561de1aaea35b
poudyalamit <poudyalamit20@gmail.com>
2023-12-01 22:30:40 +0545

    added resources

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+      // Create a Leaflet map instance if it doesn't exist
+      if (!mapRef.current) {
+        mapRef.current = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors',
+        }).addTo(mapRef.current);
+      }
+    }, []); 
   return (
-    <div>
-      
-    </div>
+    <div id="map" style={{ height: '100vh', width: '100%' }}></div>
   );
 }
 
