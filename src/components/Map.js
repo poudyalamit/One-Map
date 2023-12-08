e4a7ee0ade44a7755b41147f137b1c2ce589fa64
poudyalamit <poudyalamit20@gmail.com>
2023-12-08 23:03:09 +0545

    added layer control

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { LayersControl, MapContainer, TileLayer } from 'react-leaflet';
+// require('dotenv').config();
 const Map = () => {
     return (
         <MapContainer
@@ -15,6 +16,16 @@ const Map = () => {
                 attribution='© OpenStreetMap contributors'
                 maxZoom={30}
             />
+
+            <LayersControl position='topright'>
+                <LayersControl.BaseLayer name='satellite'>
+                <TileLayer url='https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'/>
+                </LayersControl.BaseLayer>
+
+                <LayersControl.BaseLayer name='OSM'>
+                <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+                </LayersControl.BaseLayer>
+            </LayersControl>
         </MapContainer>
     );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,9 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 const Navbar = () => {
   return (
-    <div className="bg-white p-4 shadow fixed w-full flex justify-between items-center">
-    <Link href="#" id="logo" className="text-blue-500 text-lg font-semibold">Your Maps</Link>
-    <div className="flex-grow mx-4 flex">
-        <input type="text" id="search-input" placeholder="Search..." className="border-none p-2 flex-grow"/>
-        <button id="search-btn" className="bg-blue-500 text-white p-2 cursor-pointer">Search</button>
-    </div>
+<div >
+<p className='text-xl m-5'>jdsjdgsghd</p>
+
 </div>
   )
 }
